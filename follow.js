74399381dbf617a7519643fd1cb38c2bace90a24
KJ Samora <kjsamora@gmail.com>
2017-11-05 11:36:21 -0700

    created twitter bot

---
+++ follow.js
@@ -0,0 +1,27 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+var params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'popular',
+  lang: 'en'
+}
+
+T.get('search/tweets', params, function(err, data, response) {
+  if(!err){
+    for(let i = 0; i < data.statuses.length; i++){
+      let screen_name = data.statuses[i].user.screen_name;
+      T.post('friendships/create', {screen_name}, function(err, response){
+        if(err){
+          console.log(err);
+        } else {
+          console.log(screen_name, ': **FOLLOWED**');
+        }
+      });
+    }
+  } else {
+    console.log(err);
+  }
+})
